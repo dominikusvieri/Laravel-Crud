18663f919aac79061ee33c6ff6741df9854f1632
dominikusvieri <dominikusvieri9@gmail.com>
2022-10-20 10:28:22 +0700

    hero1 name

--- src/App.java
+++ src/App.java
@@ -17,7 +17,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Hero hero1 = new Hero();
-        hero1.name = "Andi";
+        hero1.name = "Yosi";
         // System.out.println("Hero 1 : "+hero1.name);
         hero1.display();
 
